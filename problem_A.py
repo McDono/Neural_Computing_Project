fc55a706484faa7858ec16f5ec5b42705468d4aa
Donovan <mcdonovan@outlook.fr>
2019-09-30 19:11:58 -0500

    cleaning

--- problem_A.py
+++ problem_A.py
@@ -1,43 +1,30 @@
-inputON = [
-	{1,1},
-	{2,1},
-	{1,2},
-	{2,2},
-	{1.5,1.5}
-]
-inputOFF = [
-	{4,4},
-	{4,5},
-	{5,4},
-	{5,5},
-	{4.5,4.5}
-]
-W = [0, 0]
-b = 0
-error = 0
 
-NOMBRE_EPOCH = 10
+import numpy as np
+import functions as fn
 
-def hardlims(input):
-	if (input >= 0):
-		return 1
-	else:
-		return -1
+inputOFF = np.array([[1.0, 2.0, 1.0, 2.0, 1.5],
+					[1.0, 1.0, 2.0, 2.0, 1.5]])
+inputON = np.array([[4.0, 4.0, 5.0, 5.0, 4.5],
+					 [4.0, 5.0, 4.0, 5.0, 4.5]])
+# inputTotal = np.array(inputON, inputOFF)
+global W
+W = np.array([0.0, 0.0])
+global b
+b = 0.0
+# global error
+# error = 0.0
 
-def matrix_product(A,B):
-	product = []
-	finalComponent = []
-	if (len(A[0]) != len(B)):
-		return False
-	else:
-		for i in range(0, len(A)-1):
-			finalComponent = 0
-			for j in range(0, len(B)-1):
-				finalComponent += A[i][j]*B[i][j]
+NOMBRE_EPOCH = 4
 
-def display_Matrix(A):
-	for i in range(0, len(A)-1):
-		for j in range(0, len(A[0])-1):
-			print(A[i][j])
 
-# def run_perceptron(input)
+#--- DEBUG
+
+#--- MAIN
+for epoch in range(1, NOMBRE_EPOCH+1):
+	print("EPOCH : #" + str(epoch))
+	print("////////OFF TERMS /////////")
+	for offTerm in range(0, len(inputOFF[0])):
+		(W, b) = fn.run_perceptron(inputOFF, offTerm, -1, W, b, "hardlims")
+	print("////////ON TERMS /////////")
+	for onTerm in range(0, len(inputON[0])):
+		(W, b) = fn.run_perceptron(inputON, onTerm, 1, W, b, "hardlims")
