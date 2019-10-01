30ab00f84ede04a6313080ce3685f617838d041d
Donovan <mcdonovan@outlook.fr>
2019-09-30 19:09:26 -0500

    added some modularity; code still works for problem A

---
+++ functions.py
@@ -0,0 +1,48 @@
+import numpy as np
+import math
+
+def run_neuron(input, target, W, b, transferFunction):
+	n = np.dot(W, input) + b
+	a = apply_transfer_function(transferFunction, n)
+	error = target - a
+	return error
+
+def run_perceptron(inputTab, inputNumber, target, W, b, transferFunction):
+	inputVector = np.array([inputTab[0][inputNumber], inputTab[1][inputNumber]])
+	print("inputVector : " + str(inputVector))
+	error = run_neuron(inputVector, target, W, b, transferFunction)
+	print("error = " + str(error))
+	W = W + error*0.5*inputVector.T
+	b = b + error*0.5
+	print("input #" + str(inputNumber))
+	print("W = " + str(W))
+	print("b = " + str(b))
+	return (W, b)
+
+def apply_transfer_function(transferFunction, n):
+	if transferFunction == "hardlim":
+		return hardlim(n)
+	elif transferFunction == "hardlims" or transferFunction == "sign":
+		return hardlims(n)
+	elif transferFunction == "sigmoid":
+		return sigmoid(n)
+	elif transferFunction == "tanh":
+		return np.tanh(n)
+	else:
+		print("Transfer function unknown")
+		sys.exit()
+
+def hardlim(input):
+	if (input >= 0):
+		return 1.0
+	else:
+		return 0.0
+
+def hardlims(input):
+	if (input >= 0):
+		return 1.0
+	else:
+		return -1.0
+
+def sigmoid(n):
+  return 1 / (1 + math.exp(-n))
--- problem_A_2.py
+++ problem_A_2.py
@@ -1,6 +1,6 @@
 
-
 import numpy as np
+import functions as fn
 
 inputOFF = np.array([[1.0, 2.0, 1.0, 2.0, 1.5],
 					[1.0, 1.0, 2.0, 2.0, 1.5]])
@@ -16,29 +16,6 @@ b = 0.0
 
 NOMBRE_EPOCH = 4
 
-def hardlims(input):
-	if (input >= 0):
-		return 1.0
-	else:
-		return -1.0
-
-def run_neuron(input, target, W, b):
-	n = np.dot(W, input) + b
-	a = hardlims(n)
-	error = target - a
-	return error
-
-def run_perceptron(inputTab, inputNumber, target, W, b):
-	inputVector = np.array([inputTab[0][inputNumber], inputTab[1][inputNumber]])
-	print("inputVector : " + str(inputVector))
-	error = run_neuron(inputVector, target, W, b)
-	print("error = " + str(error))
-	W = W + error*0.5*inputVector.T
-	b = b + error*0.5
-	print("input #" + str(inputNumber))
-	print("W = " + str(W))
-	print("b = " + str(b))
-	return (W, b)
 
 #--- DEBUG
 
@@ -47,7 +24,7 @@ for epoch in range(1, NOMBRE_EPOCH+1):
 	print("EPOCH : #" + str(epoch))
 	print("////////OFF TERMS /////////")
 	for offTerm in range(0, len(inputOFF[0])):
-		(W, b) = run_perceptron(inputOFF, offTerm, -1, W, b)
+		(W, b) = fn.run_perceptron(inputOFF, offTerm, -1, W, b, "hardlims")
 	print("////////ON TERMS /////////")
 	for onTerm in range(0, len(inputON[0])):
-		(W, b) = run_perceptron(inputON, onTerm, 1, W, b)
+		(W, b) = fn.run_perceptron(inputON, onTerm, 1, W, b, "hardlims")
