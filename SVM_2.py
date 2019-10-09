dfaa7e23f9b27d59ba8ac104d80c264edc2faae7
jav_solo <javiersolorzano@icloud.com>
2019-10-09 13:33:53 -0500

    Update SVM_2.py

--- SVM_2.py
+++ SVM_2.py
@@ -6,8 +6,8 @@ def quadprog_solve_qp(P, q, G=None, h=None, A=None, b=None):
     qp_G = .5 * (P + P.T)   # make sure P is symmetric
     qp_a = -q
     if A is not None:
-        qp_C = -numpy.vstack([A, G]).T
-        qp_b = -numpy.hstack([b, h])
+        qp_C = -np.vstack([A, G]).T
+        qp_b = -np.hstack([b, h])
         meq = A.shape[0]
     else:  # no equality constraint
         qp_C = -G.T
